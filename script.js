027b0597e09ca11fe208f8eae456ee4bdcc08e28
louismjn147-crypto <louismjn147@gmail.com>
2026-07-20 15:12:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
             date: "Mardi 17 septembre",
             time: "21h00",
             homeTeam: "Paris SG",
-            homeLogo: "logos/Congo.png",
+            homeLogo: "logos/psg.png",
             awayTeam: "Real Madrid",
             awayShort: "RMA",
             odds: {
